55f8a7684c9edfafffc812277745e11aa712af07
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-06-05 21:36:46 +0300

    Entity definition , Fetch - EAGER / LAZY

---
+++ 05-JPA-Hibernate/src/main/java/mehmetberkan/JPAHibernate/model/Lesson.java
@@ -0,0 +1,57 @@
+package mehmetberkan.JPAHibernate.model;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+public class Lesson {
+	
+	@Id
+	@Column(name = "student_id")
+	private int id;
+	
+	private String name;
+	
+	private LocalDate registrationDate;
+	
+	@ManyToMany(mappedBy = "registeredLessons", fetch = FetchType.EAGER)   //EAGER -  Getir
+	private Set<Student> students;										   //LAZY  -  Gerekli olduğu zaman getir
+
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDate registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public Set<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+}
