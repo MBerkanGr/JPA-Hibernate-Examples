0510a3b96c055af1de221847d1cecc61369098bb
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-06-08 23:59:29 +0300

    Entity definition , added hibernate.cfg.xml , done mysql connections

--- 07-JPA-Hibernate/src/main/java/mehmetberkan/JPAHibernate/App.java
+++ 07-JPA-Hibernate/src/main/java/mehmetberkan/JPAHibernate/App.java
@@ -1,5 +1,28 @@
 package mehmetberkan.JPAHibernate;
 
-public class App {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+import mehmetberkan.JPAHibernate.model.Student;
+
+
+public class App {
+    
+	public static void main( String[] args ) {
+		SessionFactory factory = new Configuration() 
+						.configure("hibernate.cfg.xml")
+						.addAnnotatedClass(Student.class)
+						.buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		try {
+			Student student = new Student("Tuğrul","Bayrak", "mail@mail.com");
+			session.beginTransaction();
+			session.save(student);
+			session.getTransaction().commit();
+			
+		} finally {
+			factory.close();
+		}	
+    }
 }
--- 07-JPA-Hibernate/src/main/java/mehmetberkan/JPAHibernate/util/HibernateUtil.java
+++ 07-JPA-Hibernate/src/main/java/mehmetberkan/JPAHibernate/util/HibernateUtil.java
@@ -3,6 +3,7 @@ package mehmetberkan.JPAHibernate.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
 public class HibernateUtil {
 
 	private static final SessionFactory sessionFactory = buildSessionFactory();
