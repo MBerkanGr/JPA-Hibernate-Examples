1d4782cbdb45daa3931618f0883fb6fe2554a31b
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-06-09 23:58:17 +0300

    Entity definition , added hibernate.cfg.xml , done mysql connections

--- 07-JPA-Hibernate/src/main/java/mehmetberkan/JPAHibernate/App.java
+++ 07-JPA-Hibernate/src/main/java/mehmetberkan/JPAHibernate/App.java
@@ -3,7 +3,6 @@ package mehmetberkan.JPAHibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
 import mehmetberkan.JPAHibernate.model.Student;
 
 
@@ -16,7 +15,7 @@ public class App {
 						.buildSessionFactory();
 		Session session = factory.getCurrentSession();
 		try {
-			Student student = new Student("Tuğrul","Bayrak", "mail@mail.com");
+			Student student = new Student("Mehmet","Gr", "mbgr@mail.com");
 			session.beginTransaction();
 			session.save(student);
 			session.getTransaction().commit();
